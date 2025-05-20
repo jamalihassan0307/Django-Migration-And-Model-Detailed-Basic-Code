ffcd43c0a35891245bd85c8f0e8ce6dd5de9c471
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-20 23:16:17 +0500

    Add unit tests for login view, ensuring the login page loads correctly and uses the appropriate template. Verify the presence of key elements on the page.

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+
+    def test_login_page_loads(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+        self.assertContains(response, 'Login')
+        self.assertContains(response, 'g_id_onload')
